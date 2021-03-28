f2f43239042c758c17534ac0d96984041dc1a76a
anarusadze <ana.rusadze.1@btu.edu.ge>
2021-03-28 16:13:29 +0400

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,23 @@
+fun main() {
+    val number = 327
+    print("შედეგი: ${getResult(number)}")
+}
+
+fun getResult(number: Int): Int {
+
+    var num = number
+    var sum = 0
+    var multiplication = 1
+
+    while (num != 0) {
+        sum += num % 10
+        multiplication *= num % 10
+        num /= 10
+    }
+
+    print("ნამრავლი: $multiplication ")
+    print("ჯამი: $sum ")
+
+    return multiplication - sum
+
+}
